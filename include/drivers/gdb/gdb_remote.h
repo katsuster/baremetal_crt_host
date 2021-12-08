973a8d2d233174bddcfa91f009232e8cd409ca31
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2021-12-08 19:05:27 +0900

    drivers: add directory for gdb remote protocol

---
+++ include/drivers/gdb/gdb_remote.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef BAREMETAL_CRT_HOST_DRV_GDB_REMOTE
+#define BAREMETAL_CRT_HOST_DRV_GDB_REMOTE
+
+#include <in_cl.h>
+
+#endif /* BAREMETAL_CRT_HOST_DRV_GDB_REMOTE */
