3bbe1c7ae4149bb881dc453143dfbc91e72f744f
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2021-12-24 17:26:06 +0900

    cl: add log functions

--- include/type_cl.h
+++ include/type_cl.h
@@ -9,4 +9,18 @@
 
 #define ARRAY_OF(x)    (sizeof(x) / sizeof((x)[0]))
 
+#define LOG_HEADER    "%s:%d: "
+#define log_info(fmt, ...) \
+	do { \
+		fprintf(stdout, LOG_HEADER fmt, __func__, __LINE__, ##__VA_ARGS__); \
+	} while (0)
+#define log_warn(fmt, ...) \
+	do { \
+		fprintf(stdout, LOG_HEADER fmt, __func__, __LINE__, ##__VA_ARGS__); \
+	} while (0)
+#define log_err(fmt, ...) \
+	do { \
+		fprintf(stdout, LOG_HEADER fmt, __func__, __LINE__, ##__VA_ARGS__); \
+	} while (0)
+
 #endif /* BAREMETAL_CRT_HOST_TYPE_CL */
