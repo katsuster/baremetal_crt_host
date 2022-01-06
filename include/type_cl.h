8742e1b3aba83579ef7d34b3affeb1e13d2913e2
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-01-07 01:41:15 +0900

    cl: add log functions for debug out

--- include/type_cl.h
+++ include/type_cl.h
@@ -10,6 +10,10 @@
 #define ARRAY_OF(x)    (sizeof(x) / sizeof((x)[0]))
 
 #define LOG_HEADER    "%s:%d: "
+#define log_dbg(fmt, ...) \
+	do { \
+		fprintf(stdout, LOG_HEADER fmt, __func__, __LINE__, ##__VA_ARGS__); \
+	} while (0)
 #define log_info(fmt, ...) \
 	do { \
 		fprintf(stdout, LOG_HEADER fmt, __func__, __LINE__, ##__VA_ARGS__); \
